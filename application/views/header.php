e67ebe57f1f8a332905a6d06eeebfbe5f6bf0fdd
Dibouba <diallobouba905@gmail.com>
2021-03-14 19:05:16 +0100

    commit fil rouge

---
+++ application/views/header.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="<?php echo base_url("assets/css/style.css");?>">
+    <link rel="icon" type="image/png" href="<?php echo base_url("assets/images/git.png");?>">
+    <title>Village green</title>
+</head>
+<body>
+    
+        
+<div class="container">
+    <!--<div class="row">-->
+     <header>
+        <!--<div class="col-9 col-md-12 col-sm-12 ml-3 mt-3">
+           
+        </div>
+            <div class="col-3 ml-1">
+                </div> -->
+         <nav class="navbar navbar-expand-sm navbar-light">
+              <a href="index.php"><img src="<?php echo base_url("assets/images/HEADER/logo_village_green.png"); ?>"  classe="img-fluid mt-2" height="240" alt="image logo"></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+          
+            <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+              <ul class="navbar-nav mr-auto mt-lg-0">
+                <li class="nav-item active mr-3">
+                  <a class="nav-link" href="#">info</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Espace Client</a>
+                </li>
+            </ul>
+               
+            <div class="my-2 my-lg-0">
+                  <a href="#"><img src="<?php echo base_url("assets/images/HHEADER/picto_panier.png");?>" class="img-fluid" alt="panier"></a>
+                  <a href="#"><img src="<?php echo base_url("assets/images/HEADER/picto_pays.png");?>"  class="img-fluid" alt="pays"></a>
+              </div>
+            </div>
+         </nav>
+                <!-- <div class="row">
+                    <div class="col-12">
+                        <ul class="nav ml-9 mt-4">
+                            <li class="nav-item font-weight-bold font-family:5 mr-5"><a href="produit.php" class="text-light">produits</a></li>
+                            <li class="nav-item mr-5"><a href="service.php" class="text-light">Services</a></li>
+                            <li class="nav-item mr-5"><a href="aide.php" class="text-light">Aide</a></li>
+                            <li class="nav-item mr-5"><a href="propos" class="text-light">A propos</a></li>
+                        </ul>
+                    </div>
+               </div>
+               <div class="row">
+                   <div class="col-sm-12 col-md-8 col-lg-12">
+                        <ul class="nav mr-5 mt-4">
+                            <li class="nav-item"><a href="#" class="text-light">Guit/Bass</a></li>
+                            <li class="nav_item"><a href="#" class="text-light">Batteries</a></li>
+                            <li class="nav-item"><a href="#" class="text-light">Clavier</a></li>
+                            <li class="nav-item"><a href="#" class="text-light">Studio</a></li>
+                            <li class="nav-item"><a href="#" class="text-light">Sono</a></li>
+                            <li class="nav-item"><a href="#" class="text-light">Eclairage</a></li>
+                            <li class="nav-item"><a href="#" class="text-light">DJ</a></li>
+                            <li class="nav-item"><a href="#" class="text-light">Cases</a></li>
+                            <li class="nav-item"><a href="#" class="text-light">Accessoires</a></li>
+                        </ul>
+                   </div>
+               </div>-->
+        </div> 
+        </header>
+            
+        
+    <!--</div>-->
+   
+           
