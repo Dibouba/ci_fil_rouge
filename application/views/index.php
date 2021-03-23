59f5703cc5c4e6e9eef16ea89b72ee8a4810604b
Dibouba <diallobouba905@gmail.com>
2021-03-23 21:36:37 +0100

    flowing to do

--- application/views/index.php
+++ application/views/index.php
@@ -1,11 +1,85 @@
+<!--la mise en place des images pub guitare, le lien de savoir l'image pour information sur le mode de ivraison -->
 <div class="row mt-5">
     <div class="col-12 col-md-9">
         <a href="#">
             <img src="<?php echo base_url("assets/images/body/pub_guitare_bouton_blanc.png"); ?>" class="img-fluid position-absolute" alt="inedex">
         </a>
-        <img src="<?php echo base_url("assets/images/body/pub guitare.png"); ?>" class="img-fluid" alt="pub guitare">
+        <img src="<?php echo base_url("assets/images/body/pub guitare.png"); ?>" class="img-fluid" id="pub" alt="pub guitare">
     </div>
-    <div class="col-0 col-md-3 d-block">
+    <div class="col-0 col-md-3 d-block h-auto">
         <img src="<?php echo base_url("assets/images/body/banniere_droite_prix.png"); ?>" class="img-fluid" alt="">
+    </div>
+</div>
+<!--l'image de la banniere pour les differentes services-->
+<div class="row mt-4">
+    <div class="col-12">
+        <img src="<?php echo base_url("assets/images/body/banniere_centre_4_pictos.png"); ?>" class="img-fluid" width="1100" alt="image-banniere">
+    </div>
+</div>
+<!--la catégories des produit-->
+<div class="row mt-2">
+    <div class="col-4">
+        <h3 class="ml-4">Nos catégories</h3>
+    </div>
+</div>
+<!--la premiére ligne des produits-->
+<div class="row">
+    <!--categorie guitare-->
+    <div class="col-3 ml-1">
+        <a href="#"><img src="<?php echo base_url("assets/images/body/categories guitare.png"); ?>" id="guit" class="img-fluid" alt="cat-guitare"></a>
+    </div>
+    <!--categorie batterie-->
+    <div class="col-3 mr-1">
+        <a href="#"><img src="<?php echo base_url("assets/images/body/categories batterie.png"); ?>" id="bat" class="img-fluid" alt="cat-batterie"></a>
+    </div>
+    <!--categorie  piano-->
+    <div class="col-3">
+        <a href="#"><img src="<?php echo base_url("assets/images/body/categories piano.png"); ?>" id="piano" class="img-fluid" alt="cat-piano"></a>
+    </div>
+    <!--categorie micro-->
+    <div class="col-2">
+        <a href="#"><img src="<?php echo base_url("assets/images/body/categories micro.png"); ?>" id="micro" class="img-fluid" alt="cat-micro"></a>
+    </div>
+</div>
+<!--le deuxiéme ligne des produits-->
+<div class="row">
+    <!-- catégorie sono-->
+    <div class="col-3 ml-1">
+        <a href="#"><img src="<?php echo base_url("assets/images/body/categories sono.png"); ?>" id="sono" class="img-fluid" alt="cat-sono"></a>
+    </div>
+    <!--catégorie case-->
+    <div class="col-3">
+        <a href="#"><img src="<?php echo base_url("assets/images/body/categories cases.png"); ?>" id="case" class="img-fluid" alt="cat-case"></a>
+    </div>
+    <!--catégorie cable-->
+    <div class="col-3">
+        <a href="#"><img src="<?php echo base_url("assets/images/body/categories cable.png"); ?>" id="cable" class="img-fluid" alt="cat-cable"></a>
+    </div>
+    <!--catégorie saxo-->
+    <div class="col-2">
+        <a href="#"><img src="<?php echo base_url("assets/images/body/categories saxo.png"); ?>" id="saxo" class="img-fluid" alt="cat-micro"></a>
+    </div>
+</div>
+<!--le top de vente et les partenaires-->
+<div class="row mt-4">
+    <div class="col-6">
+        <h3 class="ml-4">Nos meilleures ventes</h3>
+    </div>
+    <div class="col-6">
+        <h3>Nos partenaires</h3>
+    </div>
+</div>
+<div class="row">
+    <div class="col-2 ml-4">
+        <a href="#"><img src="<?php echo base_url("assets/images/body/top ventes guitare.png");?>" class="img-fluid" alt="top-guitare"></a>
+    </div>
+    <div class="col-2">
+        <a href="#"><img src="<?php echo base_url("assets/images/body/top ventes saxo.png");?>" class="img-fluid" alt="top-saxo"></a>
+    </div>
+    <div class="col-2">
+        <a href="#"><img src="<?php echo base_url("assets/images/body/top ventes piano");?>" class="img-fluid" alt="top-piano"></a>
+    </div>
+    <div class="col-5">
+        <img src="<?php echo base_url("assets/images/body/partenaires 4 logos.png");?>" class="img-fluid" alt="logo-partenaire">
     </div>
 </div>
