14c3eb340503dcee97e8dd85765521e28d46e39c
Dibouba <diallobouba905@gmail.com>
2021-03-24 23:47:44 +0100

    in progress

--- application/views/index.php
+++ application/views/index.php
@@ -4,59 +4,59 @@
         <a href="#">
             <img src="<?php echo base_url("assets/images/body/pub_guitare_bouton_blanc.png"); ?>" class="img-fluid position-absolute" alt="inedex">
         </a>
-        <img src="<?php echo base_url("assets/images/body/pub guitare.png"); ?>" class="img-fluid" id="pub" alt="pub guitare">
+        <img src="<?php echo base_url("assets/images/body/pub guitare.png"); ?>"  class="img-fluid" id="pub" alt="pub guitare">
     </div>
     <div class="col-0 col-md-3 d-block h-auto">
         <img src="<?php echo base_url("assets/images/body/banniere_droite_prix.png"); ?>" class="img-fluid" alt="">
     </div>
 </div>
 <!--l'image de la banniere pour les differentes services-->
-<div class="row mt-4">
+<div class="row mt-4 ">
     <div class="col-12">
-        <img src="<?php echo base_url("assets/images/body/banniere_centre_4_pictos.png"); ?>" class="img-fluid" width="1100" alt="image-banniere">
+        <img src="<?php echo base_url("assets/images/body/banniere_centre_4_pictos.png"); ?>" class="img-fluid ml-3"  alt="image-banniere">
     </div>
 </div>
 <!--la catégories des produit-->
 <div class="row mt-2">
-    <div class="col-4">
+    <div class="col-12">
         <h3 class="ml-4">Nos catégories</h3>
     </div>
 </div>
 <!--la premiére ligne des produits-->
 <div class="row">
     <!--categorie guitare-->
-    <div class="col-3 ml-1">
+    <div class="col-6 col-md-3">
         <a href="#"><img src="<?php echo base_url("assets/images/body/categories guitare.png"); ?>" id="guit" class="img-fluid" alt="cat-guitare"></a>
     </div>
     <!--categorie batterie-->
-    <div class="col-3 mr-1">
+    <div class="col-6 col-md-3">
         <a href="#"><img src="<?php echo base_url("assets/images/body/categories batterie.png"); ?>" id="bat" class="img-fluid" alt="cat-batterie"></a>
     </div>
     <!--categorie  piano-->
-    <div class="col-3">
+    <div class="col-6 col-md-3">
         <a href="#"><img src="<?php echo base_url("assets/images/body/categories piano.png"); ?>" id="piano" class="img-fluid" alt="cat-piano"></a>
     </div>
     <!--categorie micro-->
-    <div class="col-2">
-        <a href="#"><img src="<?php echo base_url("assets/images/body/categories micro.png"); ?>" id="micro" class="img-fluid" alt="cat-micro"></a>
+    <div class="col-6 col-md-3">
+        <a href="#"><img src="<?php echo base_url("assets/images/body/categories micro.png"); ?>" id="micro" class="img-fluid w-3" alt="cat-micro"></a>
     </div>
 </div>
 <!--le deuxiéme ligne des produits-->
 <div class="row">
     <!-- catégorie sono-->
-    <div class="col-3 ml-1">
+    <div class="col-6 col-md-3">
         <a href="#"><img src="<?php echo base_url("assets/images/body/categories sono.png"); ?>" id="sono" class="img-fluid" alt="cat-sono"></a>
     </div>
     <!--catégorie case-->
-    <div class="col-3">
+    <div class="col-6 col-md-3">
         <a href="#"><img src="<?php echo base_url("assets/images/body/categories cases.png"); ?>" id="case" class="img-fluid" alt="cat-case"></a>
     </div>
     <!--catégorie cable-->
-    <div class="col-3">
+    <div class="col-6 col-md-3">
         <a href="#"><img src="<?php echo base_url("assets/images/body/categories cable.png"); ?>" id="cable" class="img-fluid" alt="cat-cable"></a>
     </div>
     <!--catégorie saxo-->
-    <div class="col-2">
+    <div class="col-6 col-md-3">
         <a href="#"><img src="<?php echo base_url("assets/images/body/categories saxo.png"); ?>" id="saxo" class="img-fluid" alt="cat-micro"></a>
     </div>
 </div>
